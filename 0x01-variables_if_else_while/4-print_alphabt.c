3d6d7b8a52f89b620cfb1fe08dcfc48662a7da6d
pro314 <apn.madlala@protonmail.com>
2023-09-08 08:01:55 -0700

    C Variables

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Print alphabets in lowercase beside q amd e.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e'&& letter != 'q')
+			putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
